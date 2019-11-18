a1caf5ef896df8ddfdfb601ebd0fa3799c226e5d
Joy Skipper <jskipper@vanir.gb.nrao.edu>
2019-11-18 15:31:33 -0500

    in search choices for home page, made those choices in a dictionary instead of hardcoded.

--- GBT_RFI_Webpage/python_django_dev/pages/views.py
+++ GBT_RFI_Webpage/python_django_dev/pages/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.choices import receiver_choices
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'pages/index.html')
+    context = {
+        'receiver_choices':receiver_choices,
+    }
+    return render(request, 'pages/index.html',context)
 
 def about(request):
     return render(request, 'pages/about.html')
