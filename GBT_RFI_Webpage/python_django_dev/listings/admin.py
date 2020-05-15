9a2237c5bdcb74a67d7b0cfdf70e70e903bdb53b
JoySkipper <jns9md@virginia.edu>
2020-05-15 16:02:55 -0400

    removed ref to old table structure

--- GBT_RFI_Webpage/python_django_dev/listings/admin.py
+++ GBT_RFI_Webpage/python_django_dev/listings/admin.py
@@ -9,8 +9,8 @@ from .models import MasterRfiCatalog
 from .models import MasterRfiFlaggedCatalog
 
 class ListingAdmin(admin.ModelAdmin):
-    list_display = ('data_ID', 'filename', 'frequency_mhz', 'intensity_jy')
-    list_display_links = ('data_ID','filename')
+    list_display = ('filename', 'frequency_mhz', 'intensity_jy')
+    list_display_links = ('filename',)
     search_fields = ('filename','frequency_mhz')
 
 admin.site.register(MasterRfiCatalog, ListingAdmin)
