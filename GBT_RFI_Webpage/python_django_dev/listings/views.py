80568c171929842a0d7ad6c2666cd8fc08397b3e
JoySkipper <jns9md@virginia.edu>
2020-05-29 10:36:06 -0400

    added comments

--- GBT_RFI_Webpage/python_django_dev/listings/views.py
+++ GBT_RFI_Webpage/python_django_dev/listings/views.py
@@ -58,6 +58,7 @@ def index(request):
 
     # right now, we're not returning the downloaded file, just the static HTML page.
     # It will take care of the downloaded file
+    # Return the listings page
     return render(request,'listings/listings.html',context)
 
 
@@ -75,30 +76,33 @@ def validate_username(request):
     return JsonResponse(username_data)
 
 def parse_query_chunk(final_query,temp_filename):
+     # This takes a very large query to the database and requests and returns it in
+     # Bite-sized chunks to as to not overload the memory
      print('in parse_query chunk')
      with connection.cursor() as cursor:
-            #writer = csv.writer(pseudo_buffer)
             cursor.execute(final_query)
-            #temp_file = open(temp_filename,"a+")
-            #row = cursor.fetchone()
-            #line_since_last_flush = 0
             with open(temp_filename,'w+') as temp_file:
                 temp_file.writelines('Frequency_MHz,Intensity_Jy\n')
             for row in cursor:
+		# The reason we are opening the file every line (which is, of course, slower) is so
+		# That we do not overload the memory by trying to maintain the whole written file to memory
                 with open(temp_filename,'a+') as temp_file:
                     txt = ''
                     for column in row:
+			# Write each column, comma-separated
                         txt += str(float(column))+', '
-                    # Removing last column of the row
+                    # Removing last ', ' of the row
                     txt = txt[:-2]
                     txt += '\n'
                     temp_file.writelines(txt)
      print('wrote chunk to '+temp_filename)
      cursor.close()
      del cursor
+     # Ensures wiping of memory:
      gc.collect()
 
 def get_final_query(semifinal_query,temp_filename,chunk_size):
+    # Takes restult from receiver table and gets final query for main table
     with connection.cursor() as cursor:
         cursor.execute(semifinal_query)
         records = cursor.fetchall()
@@ -130,14 +134,21 @@ def django_save_me(request):
     url_dict = new_url_dict
     print('checking if latest projid or no')
     start_row = 0
+    # Chunk size determines max memory load. I tailored this to vanir (4 gb ram) could be raised, and thus sped up, if allocated more
+    # Chunk size is basically how many lines to process at a time, so it doesn't directly correlate to gb. 
     chunk_size = 1000000
+    # Get temporary file to write to, so as to not hold in memory. 
     temp_path = tempfile.gettempdir()+'/'
+    # Giving it random hash at end of file so it's unique
     random.seed()
     random_temp_file_bit = random.randint(0,100000)
     temp_filename = temp_path+"temp_file_"+str(random_temp_file_bit)+".txt"
+    # If the user requested the latest project
     if 'latest_projid' in url_dict:
         filtered_queryset = latest_projects.objects.filter(frontend = url_dict['receiver']).values()[0]
+	# We get the latest project ID
         projid = filtered_queryset['projid']
+	# Now we can pull the required parameters from that project ID table
         semifinal_query = 'SELECT * from '+str(projid)
         if 'frequency_min' in url_dict:
             semifinal_query += ' WHERE Frequency_MHz > '+str(url_dict['frequency_min'])
@@ -151,6 +162,7 @@ def django_save_me(request):
                 semifinal_query = new.join(li)
         print(semifinal_query)
         final_query = get_final_query(semifinal_query,temp_filename,chunk_size)
+	# Parsing the final query in chunks so as to not overload memory
         p = multiprocessing.Process(target=parse_query_chunk,args=(final_query,temp_filename))
         print('starting multiprocess')
         p.start()
@@ -158,6 +170,7 @@ def django_save_me(request):
         gc.collect()
         print('written to '+temp_filename)
         print('is latest')
+    # If the observer did NOT want the latest project:
     else:
         # Initializing a queryset of the database table
         queryset = determine_queryset(url_dict['receiver']).getQueryset()
@@ -170,6 +183,7 @@ def django_save_me(request):
         filtered_queryset = filter_sorter(queryset,url_dict).getQueryset()
         print('got filtered queryset')
         while True:
+	    # Filtering query in chunks, to not overload memory
             filtered_rcvr_queryset = filtered_queryset.order_by('-id')[start_row:start_row+chunk_size].values()
             if not filtered_rcvr_queryset:
                 break
@@ -177,10 +191,9 @@ def django_save_me(request):
 	    
 	    
             print('have query, starting connection')
-            #start_row = 0
-            #chunk_size = 1
-            #final_query = semifinal_query + ' LIMIT '+start_row+', '+chunk_size
+	    # We're looking at the main table first, so final query is just the same query
             final_query = semifinal_query
+	    # Parse that query in chunks
             p = multiprocessing.Process(target=parse_query_chunk,args=(final_query,temp_filename))
             print('starting multiprocess')
             p.start()
@@ -201,6 +214,8 @@ def django_save_me(request):
                     break
     except FileNotFoundError:
         count = 0
+    # If data was written to the file, we return the temp_filename with all the data
+    # FileResponse already streams this back in chunks for us, hence using it over HttpResponse which would not do this. 
     if count > 1:
         response = FileResponse(open(temp_filename,'rb'))
     else:
